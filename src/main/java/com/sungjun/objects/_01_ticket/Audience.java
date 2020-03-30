8a6f2812eb9490026acdbd0a25ce9d43955babf3
Sungjun <sungjunpizz@gmail.com>
2020-03-30 20:57:31 +0900

    feat: 관람객 구현
    - 관람객은 소지품을 보관하기 위한 가방을 소지할 수 있다.

---
+++ src/main/java/com/sungjun/objects/_01_ticket/Audience.java
@@ -0,0 +1,13 @@
+package com.sungjun.objects._01_ticket;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}

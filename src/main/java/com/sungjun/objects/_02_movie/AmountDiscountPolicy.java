99215875b03a4b5da36c377741e7dc0d68a33866
Sungjun <sungjunpizz@gmail.com>
2020-04-07 23:39:37 +0900

    feat: 할인 정책 구현
    - 할인 정책 인터페이스 추가
    - 구현체인 일정 금액 할인 정책 추가
    - 구현체인 일정 비율 할인 정책 추가

---
+++ src/main/java/com/sungjun/objects/_02_movie/AmountDiscountPolicy.java
@@ -0,0 +1,15 @@
+package com.sungjun.objects._02_movie;
+
+public class AmountDiscountPolicy extends DiscountPolicy {
+    private Money discountAmount;
+
+    public AmountDiscountPolicy(Money discountAmount, DiscountCondition... conditions) {
+        super(conditions);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money getDiscountAmount(Screeing screeing) {
+        return discountAmount;
+    }
+}
---
+++ src/main/java/com/sungjun/objects/_02_movie/PercentDiscountPolicy.java
@@ -0,0 +1,15 @@
+package com.sungjun.objects._02_movie;
+
+public class PercentDiscountPolicy extends DiscountPolicy {
+    private double percent;
+
+    public PercentDiscountPolicy(double percent, DiscountCondition... conditions) {
+        super(conditions);
+        this.percent = percent;
+    }
+
+    @Override
+    protected Money getDiscountAmount(Screeing screeing) {
+        return screeing.getMovieFee().tiems(percent);
+    }
+}
